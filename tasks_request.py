23da32dbddd9752fbc1818e18061d407c09a12bc
GeenStack <hayleytops@gmail.com>
2020-03-16 12:33:52 +0300

    new function to creating reports

--- tasks_request.py
+++ tasks_request.py
@@ -1,6 +1,7 @@
 from config import tasks_url, users_url
 import requests
 from time import sleep
+from time import gmtime, strftime
 
 
 #Solved with ru.stackoverflow.com/questions/816815/
@@ -57,17 +58,34 @@ def print_user(user):#THIS FUNCTION ONLY FOR DEBUG
     print(user['lost_task'])
     print('\n\n')
 
-#So, need describe the report model
-#report include:
-# 1. name, url, creating_time
-# 2. user_company
+
+def format_tasks_report(preview, task_list):
+    if(len(task_list) == 0):
+        return "User is not have tasks"
+    for i in task_list:
+        if (len(i) > 50):
+            i = i[0:49:]+'...'
+        preview +='{}\n'.format(i)
+    return preview
+
 def report_preparing(user):
+    report_creating_date = strftime("%d.%m.%Y %H:%M", gmtime())
+    report = '{} <{}> {}\n'.format(user["name"], user["email"], report_creating_date)
+    report += '{}\n\n'.format(user["company"])
+    report += format_tasks_report('completed_tasks:\n',user['completed_tasks'])
+    report += format_tasks_report('\nLost_tasks:\n', user['lost_task'])
+    print(report)
+    return {"report_content":report, "report_date":report_creating_date}
+    
+def write_report_to_file(user, report):
     pass
+
+
 
 test_users_list = users_list_created(get_data_from_api(users_url))
 parsing_tasks_list_and_add_to_users(get_data_from_api(tasks_url), test_users_list)
 for i in test_users_list:
-    print_user(i)
+    report_preparing(i)
 #TO DO
 #Describe users
 #  1. Form users_list, use some data from requests(users_url)
